46ac57e53325936ec6c07b2657a1448bbb5a9489
Pierluigi Ingrosso <p.ingrosso@EUSOFT.IT>
2026-01-09 11:28:06 +0100

    production mode

--- wp-content/themes/apuliabluetheme/assets/src/js/book-now/book-now-main.js
+++
@@ -1,9 +0,0 @@
-import { dateFormat, momentDefaultFormat } from "../generic/utility.js";
-import { BookNow } from "./book-now.js";
-
-var $;
-
-export function bookNowMain(jquery) {
-  const bookNow = new BookNow(jquery);
-  bookNow.init();
-}
